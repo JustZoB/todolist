3d92e65065e2a6a3855ad02c2561628a38e6bd63
Dmitrii Krasnoperov <d.krasnoperov@codedrop.cc>
2019-06-24 19:48:41 +0600

    Change html appending code, connect let-s with each-other

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         Task.rename($(this).parents().eq(1));
     });
     
-    $('body').on('focusout', ".taskName", function(event) {
+    $('body').on('focusout', ".taskName", function() {
         Task.saveRename($(this).parents().eq(1));
     }); 
     $('body').on('keydown', ".taskName", function(event) {
@@ -45,10 +45,11 @@ $( document ).ready(function() {
     })
     /*----------------Hashtag-----------------*/
     $('body').on('click', ".hashtag", function() {    
-        let contant = $(this).parents().eq(2);
-        contant.find(".hashtagBlock").toggleClass("hide");
-        contant.find(".hashtagValue").focus();
+        let content = $(this).parents().eq(2);
+        content.find(".hashtagBlock").toggleClass("hide");
+        content.find(".hashtagValue").focus();
     });
+    
     $('body').on('click', ".saveHashtag", function() {
         Task.saveHashtag($(this).parents().eq(3));
     });
@@ -57,6 +58,9 @@ $( document ).ready(function() {
             Task.saveHashtag($(this).parents().eq(3));
         }
     }); 
+    /*$(':not(.container)').on('click', function() {
+        $(".hashtagBlock").addClass("hide");
+    }); */
     $('body').on('click', ".task__hashTags div", function() {
         if ($(this).hasClass("active")) {
             Task.filterHashTags_remove($(this).html());
@@ -67,8 +71,6 @@ $( document ).ready(function() {
     /*----------------------------------------*/    
 
     /*--------------localStorage------------- */
-    let prevName = "";
-    let filterTags = [];
     function lsTest(){
         let test = 'test';
         try {
@@ -80,7 +82,9 @@ $( document ).ready(function() {
         }
     }
 
-    let listState = $(".listStatuses");
+    let prevName = "",
+        filterTags = [],
+        listState = $(".listStatuses");
     if(lsTest() === true){
         let localTasks = JSON.parse(localStorage.getItem("todolist"));
         if (localTasks != null) {
@@ -95,49 +99,47 @@ $( document ).ready(function() {
         add: function (name, listState) {
             htmlTask(name, listState);
     
-            let allTasks = JSON.parse(localStorage.getItem("todolist"));
+            let allTasks = JSON.parse(localStorage.getItem("todolist")),
+                localTask = {};
             if (allTasks == null) {
                 allTasks = [];
             }
-            let localTask = {};
             localTask.name = name;
             localTask.state = ".listStatuses";
             allTasks.push(localTask);
             localStorage.setItem("todolist", JSON.stringify(allTasks));
         },
 
-        rename: function (contant) {  
-            contant.find(".taskName").removeAttr("readonly").css("cursor", "text").addClass("active");
-            prevName = contant.find(".taskName").val();
-            contant.find(".taskName").focus();
+        rename: function (content) {  
+            content.find(".taskName").removeAttr("readonly").css("cursor", "text").addClass("active");
+            prevName = content.find(".taskName").val();
+            content.find(".taskName").focus();
         },
 
-        saveRename: function (contant) { 
-            contant.find(".taskName").attr("readonly", '').css("cursor", "default").removeClass("active");    
+        saveRename: function (content) { 
+            content.find(".taskName").attr("readonly", '').css("cursor", "default").removeClass("active");    
 
-            localStorageSaveRename(contant.parent());
+            localStorageSaveRename(content.parent());
         },
 
         saveHashtag: function(task) {
-            let hashtagName = "#" + task.find(".hashtagValue").val();
-            let hashtagsBlock = task.find(".task__hashTags");
-            let datalist = task.parents().find("#hashtags");
+            let hashtagName = "#" + task.find(".hashtagValue").val(),
+                hashtagsBlock = task.find(".task__hashTags"),
+                datalist = task.parents().find("#hashtags");
             if ((hashtagName != "#") && (hashtagsBlock.find("div:contains('" + hashtagName + "')").length == 0)) { 
                 task.find(".hashtagBlock").focus();
                 if ((task.find(".task__hashTags").length == 0)) {
-                    $("<div/>", {
-                        class: 'task__hashTags',
-                    }).appendTo(task);
+                    $(task).append(`<div class="${ task__hashTags }"></div>`);
                 } 
-                $("<div>" + hashtagName + "</div>").appendTo(task.find(".task__hashTags"));
+                $(hashtagBlock).append(`<div>${ hashtagName }</div>`);
                 if ($("[value='" + hashtagName.substr(1) + "']").length == 0) {
-                    $("<option value='" + hashtagName.substr(1) + "'></option>").appendTo(datalist);
+                    $(datalist).append(`<option value='" + ${ hashtagName.substr(1) } + "'></option>`);
                 }
                 task.find(".hashtagValue").val("");
 
-                let allTasks = JSON.parse(localStorage.getItem("todolist"));
-                let localHashtags = [];
-                let taskName = task.find(".taskName").val();
+                let allTasks = JSON.parse(localStorage.getItem("todolist")),
+                    localHashtags = [],
+                    taskName = task.find(".taskName").val();
                 for (let i = 0; i < allTasks.length; i++) {
                     if (allTasks[i].name == taskName) {
                         if (allTasks[i].tags == undefined) {
@@ -169,8 +171,8 @@ $( document ).ready(function() {
         moveTo: function (li, newState) {
             li.detach().appendTo($(newState));
             
-            let taskName = li.find(".taskName").val();
-            let allTasks = JSON.parse(localStorage.getItem("todolist"));
+            let taskName = li.find(".taskName").val(),
+                allTasks = JSON.parse(localStorage.getItem("todolist"));
             for (let i = 0; i < allTasks.length; i++) {
                 if (allTasks[i].name == taskName) {
                     allTasks[i].state = newState;
@@ -181,11 +183,11 @@ $( document ).ready(function() {
 
         filterHashTags_add: function (tag) {
             filterTags.push(tag);
-            let container = $(".container");
-            let taskFilter = container.find("li.task");
+            let container = $(".container"),
+            taskFilter = container.find("li.task");
             taskFilter.addClass("hashtagFilter_hide");
 
-            taskFilter.each(function(key,elem) {
+            taskFilter.each(function(key, elem) {
                 let c = 0;
                 for (let i = 0; i < filterTags.length; i++) {
                     let tagName = $(elem).find("div div div:contains('" + filterTags[i] + "')");
@@ -202,8 +204,8 @@ $( document ).ready(function() {
         },
 
         filterHashTags_remove: function (tag) {
-            let container = $(".container");
-            let taskFilter = container.find("li.task");
+            let container = $(".container"),
+                taskFilter = container.find("li.task");
             taskFilter.removeClass("hashtagFilter_hide");
             let visable = container.find("div div div:contains('" + tag + "')");
             visable.each(function(i,elem) {}).removeClass("active");
@@ -223,99 +225,47 @@ $( document ).ready(function() {
     function htmlTask(name, listState, tags) {
         let list = $(listState);
         
-        $("<li/>", {
-            class: 'task'
-        }).appendTo(list);
-        let li = list.find(".task:last-child");
+        $(list).append(`<li class="task">
+        <div class="task__color"></div>
+        <div class="task__content"></div>
+        </li>`);
+        let li = list.find(".task:last-child"),
+            content = li.find(".task__content");
 
-        $("<div/>", {
-            class: 'task__color',
-        }).appendTo(li);
-        $("<div/>", {
-            class: 'task__contant',
-        }).appendTo(li);
+        if (content.parents().eq(1).hasClass("listDone")) {
+            $(content).append(`<div class='task__name'>
+            <button class='check checked'><i class='fas fa-check-square fa_lg'></i></button>
+            <input class='taskName' type='text' value='${ name }' readonly></input>
+            </div>`);
+        }
+        else {
+            $(content).append(`<div class='task__name'>
+            <button class='check'><i class='far fa-square fa_lg'></i></button>
+            <input class='taskName' type='text' value='${ name }' readonly></input>
+            </div>`);
+        }
 
-        let contant = li.find(".task__contant");
+        $(content).append(`<div class='task__menu'>
+        <div class='task__menu__buttons'>
+        <button class='hashtag'><i class='fas fa-hashtag fa_lg'></i></button>
+        </div>
+        <div class='task__menu__change'>
+        <div class='hashtagBlock hide'>
+        <input class='hashtagValue active' type='text' list='hashtags'></input>
+        <button class='saveHashtag'><i class='fas fa-check-circle fa_lg'></i></button>
+        </div></div></div>`);
 
-        htmlTaskName(contant, name);
-        htmlTaskMenu(contant);
         if (tags != undefined) {
-            $("<div/>", {
-                class: 'task__hashTags',
-            }).appendTo(contant);
-            let hashtagBlock = li.find($(".task__hashTags"));
-            let datalist = contant.parents().eq(2).find("#hashtags");
+            $(content).append(`<div class="task__hashTags"></div>`);
+            let hashtagBlock = li.find($(".task__hashTags")),
+                datalist = content.parents().eq(2).find("#hashtags");
             for (let i = 0; i < tags.length; i++) {
-                $("<div>" + tags[i] + "</div>").appendTo(hashtagBlock);
+                $(hashtagBlock).append(`<div>${ tags[i] }</div>`);
                 if ($("[value='" + tags[i].substr(1) + "']").length == 0) {
-                    $("<option value='" + tags[i].substr(1) + "'></option>").appendTo(datalist);
+                    $(datalist).append(`<option value='" + ${ tags[i].substr(1) } + "'></option>`);
                 }
             }
         }
-    }
-
-    function htmlTaskMenu(place) {
-        $("<div/>", {
-            class: 'task__menu',
-        }).appendTo(place);
-        let menu = place.find(".task__menu");
-
-        $("<div/>", {
-            class: 'task__menu__buttons',
-        }).appendTo(menu);
-        let buttons_block = menu.find(".task__menu__buttons");
-        createButton(buttons_block, "hashtag", "fas fa-hashtag");
-        
-        $("<div/>", {
-            class: 'task__menu__change',
-        }).appendTo(menu);
-        let change_block = menu.find(".task__menu__change");
-        htmlHashtagCreating(change_block);
-    }
-
-    function htmlTaskName(place, name) {
-        $("<div/>", {
-            class: 'task__name',
-        }).appendTo(place);
-        let nameBlock = place.find(".task__name");
-
-        createButton(nameBlock, "check", "");
-        if (place.parents().eq(1).hasClass("listDone")) {
-            nameBlock.find(".check").addClass("checked");
-            nameBlock.find(".check i").addClass("fas fa-check-square");
-        }
-        else {
-            nameBlock.find(".check i").addClass("far fa-square");
-        }
-        $("<input/>", {
-            type: 'text',
-            value: name,
-            class: 'taskName',
-            readonly: ''
-        }).appendTo(nameBlock);
-    }
-
-    function htmlHashtagCreating(place) {
-        $("<div/>", {
-            class: 'hashtagBlock hide',
-        }).appendTo(place);
-        let hashtagBlock = place.find(".hashtagBlock");
-        $("<input/>", {
-            type: 'text',
-            list: 'hashtags',
-            class: 'hashtagValue active' 
-        }).appendTo(hashtagBlock);
-        createButton(hashtagBlock, "saveHashtag", "fas fa-check-circle");
-    }
-
-    function createButton(place, buttonClass, i_icon) {
-        $("<button/>", {
-            class: buttonClass
-        }).appendTo(place);
-        let button = place.find(" ." + buttonClass);
-        $("<i/>", {
-            class: i_icon + ' fa-lg'
-        }).appendTo(button);
     }
 
     function replaceCheckbox(checkbox) {
@@ -324,20 +274,24 @@ $( document ).ready(function() {
     }
 
     function findClass(listClasses) {
-        let listState = ".listStatuses"
-        if (listClasses.indexOf('listPending') >= 0) listState = ".listPending";
-        if (listClasses.indexOf('listCancel') >= 0) listState = ".listCancel";
-        if (listClasses.indexOf('listDone') >= 0) listState = ".listDone";
+        let listState = ".listStatuses";
+        if (listClasses.indexOf('listPending') >= 0) {
+            listState = ".listPending";
+        } else if (listClasses.indexOf('listCancel') >= 0) {
+            listState = ".listCancel";
+        } else if (listClasses.indexOf('listDone') >= 0) {
+            listState = ".listDone";
+        }
         return listState;
     }
 
     function localStorageOrder(li, newState) {
-        let prevLiName = li.prev().find(".taskName").val();
-        let liName = li.find(".taskName").val();
-        let allTasks = JSON.parse(localStorage.getItem("todolist"));
-        let positionToSet = 0;
-        let positionThatSet = 0;
-        let thatToSet = {};
+        let prevLiName = li.prev().find(".taskName").val(),
+            liName = li.find(".taskName").val(),
+            allTasks = JSON.parse(localStorage.getItem("todolist")),
+            positionToSet = 0,
+            positionThatSet = 0,
+            thatToSet = {};
         for (let i = 0; i < allTasks.length; i++) {
             if (allTasks[i].name == liName) {
                 allTasks[i].state = newState;
@@ -360,8 +314,8 @@ $( document ).ready(function() {
     }
 
     function localStorageSaveRename(li) {
-        let allTasks = JSON.parse(localStorage.getItem("todolist"));
-        let taskName = li.find(".taskName").val();
+        let allTasks = JSON.parse(localStorage.getItem("todolist")),
+            taskName = li.find(".taskName").val();
         for (let i = 0; i < allTasks.length; i++) {
             if (allTasks[i].name == prevName) {
                 allTasks[i].name = taskName;
